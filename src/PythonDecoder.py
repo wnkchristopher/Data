b3dcc95bae10d88b87e7d8f7e53ae2662438f3ae
Christopher Wink <christopw@student.uia.no>
2019-10-15 15:28:51 +0200

    Update decoder to write multiply puzzle

--- src/PythonDecoder.py
+++ src/PythonDecoder.py
@@ -1,19 +1,41 @@
 import solvedPuzzle_pb2
 import sys
 
+from src.Square import Square
+from src.SquareMatrix import SquareMatrix
+
+
 class WriteSolvedPuzzle:
     def __init__(self):
-        matrix = solvedPuzzle_pb2.Puzzle()
+        protoPuzzles = solvedPuzzle_pb2.Solutions()
 
         f = open("puzzle_solved.bin", "rb")
-        matrix.ParseFromString(f.read())
+        protoPuzzles.ParseFromString(f.read())
         f.close()
 
-        self.createTextFile(matrix.puzzle)
+        self.createTextFile(protoPuzzles)
 
-    def createTextFile(self, puzzle):
+    def createTextFile(self, protoPuzzles):
         f = open("../puzzle_solved.txt", "w+")
-        f.write(puzzle)
+        output = ""
+        quantity = protoPuzzles.numberOfPuzzles
+        output = "puzzles " + str(quantity) + "\n"
+
+        for puzzle in protoPuzzles.puzzles:
+            strPuzzle = "size " + str(puzzle.size) + "x" + str(puzzle.size) + "\n"
+            squareMatrix = SquareMatrix(puzzle.size)
+            squareMatrix.allSquares = []
+            for square in puzzle.squares:
+                squareMatrix.addSquare(square.x, square.y, square.value)
+            for y in range(1, puzzle.size+1):
+                for x in range(1, puzzle.size+1):
+                    strPuzzle += str(squareMatrix.getSquare(x, y).getValue()) + " "
+                strPuzzle+="\n"
+            output += strPuzzle
+
+
+
+        f.write(output)
         f.close()
 
 solvedMatrix = WriteSolvedPuzzle()
